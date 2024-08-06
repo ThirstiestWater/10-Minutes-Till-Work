c4cc222b125f0d2946b1ec39249d8a65667673fb
Sean de Koning <1dekoningsean@gmail.com>
2024-08-06 13:37:36 -0600

    Made Game Start

--- src/main.py
+++ src/main.py
@@ -1,56 +1,61 @@
-import pygame, math
+import pygame, math, sys
+from pygame.locals import *
 import data.entities
 import data.utils
-from pygame.locals import *
 from random import randint
 
-#Initialize Game
-pygame.init()
+class Game():
+    def __init__(self):
+        #Initialize Game
+        pygame.init()
 
-#Set up Clock
-FPS = 60
-clock = pygame.time.Clock()
+        #Set up Clock
+        self.FPS = 60
+        self.clock = pygame.time.Clock()
 
-#Set up Joysticks
-pygame.joystick.init()
-joysticks = [pygame.joystick.Joystick(i) for i in range(pygame.joystick.get_count())]
+        #Set up Joysticks
+        pygame.joystick.init()
+        self.joysticks = [pygame.joystick.Joystick(i) for i in range(pygame.joystick.get_count())]
 
-#Set up Display
-world_w, world_h = 1400, 700
-screen = pygame.display.set_mode((world_w, world_h))
-pygame.display.set_caption("Project")
-background = (15, 15, 30)
-margins = 10
+        #Set up Display
+        self.world_w, self.world_h = 1400, 700
+        self.screen = pygame.display.set_mode((self.world_w, self.world_h))
+        pygame.display.set_caption("Project")
+        self.background = (15, 15, 30)
 
-#Run Game
-Running = True
-while Running:
-    seconds_timer = 0
-    seconds_tracker = 0
-    playing = True
-    #Run main scene
-    while playing:
-        screen.fill(background)
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT or event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-                    Running = False
-                    playing = False
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_a:
-                    print("'a' pressed")
-            if event.type == pygame.KEYUP:
-                if event.key == pygame.K_a:
-                    print("'a' pressed")
-
-        if seconds_tracker % (FPS//2) == 0:
-             f"Half a second has passed"
-
-        if seconds_tracker == FPS:
+    def run(self):
+        #Run Game
+        Running = True
+        while Running:
+            seconds_timer = 0
             seconds_tracker = 0
-            seconds_timer += 1
-        seconds_tracker += 1
+            playing = True
+            #Run main scene
+            while playing:
+                self.screen.fill(self.background)
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT or event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                            Running = False
+                            playing = False
+                    if event.type == pygame.KEYDOWN:
+                        if event.key == pygame.K_a:
+                            print("'a' pressed")
+                    if event.type == pygame.KEYUP:
+                        if event.key == pygame.K_a:
+                            print("'a' pressed")
 
-        pygame.display.update()
-        clock.tick(FPS)
+                if seconds_tracker % (self.FPS//2) == 0:
+                    f"Half a second has passed"
 
-pygame.quit()
+                if seconds_tracker == self.FPS:
+                    seconds_tracker = 0
+                    seconds_timer += 1
+                seconds_tracker += 1
+
+                pygame.display.update()
+                self.clock.tick(self.FPS)
+
+        pygame.quit()
+        sys.exit()
+
+Game().run()
