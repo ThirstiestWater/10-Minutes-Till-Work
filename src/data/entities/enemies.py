50086eb08648275c86c4b64fbb23d18053e27af3
youngconnorr <134354392+youngconnorr@users.noreply.github.com>
2024-08-06 14:54:53 -0600

    created enemy logic

--- src/data/entities/enemies.py
+++ src/data/entities/enemies.py
@@ -1,4 +1,5 @@
 import pygame
+from utils import normalize_vector as nv
 
 
 class Enemies():
@@ -20,7 +21,6 @@ class Enemies():
     def render(self):
         ice_color = (254,250,224)
         
-        
         real_pos = [self.pos[0] + self.game.camera.get_offset()[0], self.pos[1] + self.game.camera.get_offset()[1]]
         
         
@@ -29,10 +29,20 @@ class Enemies():
         
         
     def update(self):
+        self.pos[0] += self.direction[0] * self.speed 
+        self.pos[1] += self.direction[1] * self.speed
+    
         #decrement coffee size based off of health percentage
         if self.health_percentage > (self.health / self.MAX_HEALTH):
             self.health_percentage -= 0.01
     
     
-    # def follow_player():
+    def follow_player(self):
+        distance_from_player_x = self.pos[0] - self.game.player.pos[0]
+        
+        distance_from_player_y = self.pos[1] - self.game.player.pos[1]
+        
+        xy = [distance_from_player_x, distance_from_player_y]
+        
+        self.direction = nv(xy)
         
