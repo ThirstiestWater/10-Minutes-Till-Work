cbd949b803d3158ddf639497fd7d9fb044bb1577
Sean de Koning <1dekoningsean@gmail.com>
2024-08-07 15:53:13 -0600

    Enemies are now squares that (roughly) face the player

--- src/data/entities/enemies.py
+++ src/data/entities/enemies.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame, math
 
 class Enemies():
     
@@ -6,11 +6,10 @@ class Enemies():
     
     def __init__(self, game, pos):
         self.health = self.MAX_HEALTH
-        self.health_percentage = 1
         self.damage = 1
         
         #size of character
-        self.size = game.world_w * 0.03
+        self.size = game.world_w * 0.02
         self.pos = pos
         
         self.game = game
@@ -19,13 +18,29 @@ class Enemies():
     
     def render(self):
         ice_color = (254,250,224)
+        outline = (50,130,253)
         
         real_pos = [self.pos[0] + self.game.camera.get_offset()[0], self.pos[1] + self.game.camera.get_offset()[1]]
+        offset = self.game.camera.get_offset()
         norm = [self.direction[1], self.direction[0]]
         
         #take updated ice size
-        pygame.draw.circle(self.game.screen, ice_color, real_pos, self.size * 0.9 * self.health_percentage)   
+        distance_to_corner = math.sqrt(2 * (self.size ** 2))
+        points = [[], [], [], []]
+        theta = math.atan(self.direction[1]/self.direction[0])
+        angles = [theta, theta - math.pi/2, theta - math.pi, theta - 3*math.pi/2]
+        points[0] = [(self.pos[0] + offset[0]) + (self.direction[0]*math.cos(angles[0]) - self.direction[1]*math.sin(angles[0]))*distance_to_corner, \
+                     (self.pos[1] + offset[1]) + (self.direction[0]*math.sin(angles[0]) + self.direction[1]*math.cos(angles[0]))*distance_to_corner]
+        points[1] = [(self.pos[0] + offset[0]) + (self.direction[0]*math.cos(angles[1]) - self.direction[1]*math.sin(angles[1]))*distance_to_corner, \
+                     (self.pos[1] + offset[1]) + (self.direction[0]*math.sin(angles[1]) + self.direction[1]*math.cos(angles[1]))*distance_to_corner]
+        points[2] = [(self.pos[0] + offset[0]) + (self.direction[0]*math.cos(angles[2]) - self.direction[1]*math.sin(angles[2]))*distance_to_corner, \
+                     (self.pos[1] + offset[1]) + (self.direction[0]*math.sin(angles[2]) + self.direction[1]*math.cos(angles[2]))*distance_to_corner]
+        points[3] = [(self.pos[0] + offset[0]) + (self.direction[0]*math.cos(angles[3]) - self.direction[1]*math.sin(angles[3]))*distance_to_corner, \
+                     (self.pos[1] + offset[1]) + (self.direction[0]*math.sin(angles[3]) + self.direction[1]*math.cos(angles[3]))*distance_to_corner]
+        pygame.draw.polygon(self.game.screen, ice_color, points)
+        pygame.draw.polygon(self.game.screen, outline, points, 3)
 
+        # Eyes (Functioning)
         pygame.draw.circle(self.game.screen, (0, 0, 0), [real_pos[0] + self.direction[0] * self.size * 4/10 - norm[0] * self.size*1/3, real_pos[1] + self.direction[1] * self.size*4/10 + norm[1] * self.size*1/3], self.size//6)
         pygame.draw.circle(self.game.screen, (0, 0, 0), [real_pos[0] + self.direction[0] * self.size * 4/10 + norm[0] * self.size*1/3, real_pos[1] + self.direction[1] * self.size*4/10 - norm[1] * self.size*1/3], self.size//6)
   
